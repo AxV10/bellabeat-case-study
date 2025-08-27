bba5a15e904f5c5ac8a6697d56504847b0bf50cc
AxV10 <91700049+AxV10@users.noreply.github.com>
2025-08-27 13:08:51 -0400

    Create 06_daily_analytic.sql

---
+++ sql/06_daily_analytic.sql
@@ -0,0 +1,4 @@
+SELECT SUM(CAST(1 AS INT64)) AS `cnt_daily_analytic_F22F39AF208049B3A41463495EE3E182_ok`,
+  1.1000000000000001 AS `X__alias__0`
+FROM `bellabeat-capstone-470220.BELLABEAT`.`daily_analytic` `daily_analytic`
+GROUP BY 2
